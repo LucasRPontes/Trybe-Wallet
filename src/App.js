3ee341365f3022618cd4bea91eab2dc47cc3955e
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-06-09 20:12:45 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+import './App.css';
 
 function App() {
-  return <div>Hello, TrybeWallet!!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={ () => (<Login />) } />
+        </Switch>
+      </BrowserRouter>
+    </div>
+
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main className="login-main">
+        <div className="login-box">
+          <input testid="email-input" />
+          <input data-testid="password-input" />
+          <button type="button">
+            Entrar
+          </button>
+        </div>
+      </main>
+    );
   }
 }
 
